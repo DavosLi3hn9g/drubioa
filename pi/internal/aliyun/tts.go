2316998edcf1fad837af874b18a4679b35bf359a
DavosLi3hn9g <wzt370211@yeah.net>
2021-07-27 16:28:37 +0800

    新增语音风格
    Signed-off-by: hbwind <hbfly@qq.com>

--- pi/internal/aliyun/tts.go
+++ pi/internal/aliyun/tts.go
@@ -88,10 +88,10 @@ func (isi *ClientISI) TTS(text string, fileName string) ([]byte, error) {
 }
 
 var VoiceList = []Voice{
-	{Name: "小云", Value: "Xiaoyun", Type: "标准男声", Note: "支持中文及中英文混合场景"},
+	{Name: "小云", Value: "Xiaoyun", Type: "标准女声", Note: "支持中文及中英文混合场景"},
 	{Name: "小刚", Value: "Xiaogang", Type: "标准男声", Note: "支持中文及中英文混合场景"},
-	{Name: "小梦", Value: "Xiaomeng", Type: "标准女声", Note: "支持中文及中英文混合场景"},
-	{Name: "小威", Value: "Xiaowei", Type: "标准男声", Note: "支持中文及中英文混合场景"},
+	{Name: "小梦", Value: "Xiaomeng", Type: "标准女声", Note: "支持中文及中英文混合场景"}, //del
+	{Name: "小威", Value: "Xiaowei", Type: "标准男声", Note: "支持中文及中英文混合场景"},  //del
 	{Name: "若兮", Value: "Ruoxi", Type: "温柔女声", Note: "支持中文及中英文混合场景"},
 	{Name: "思琪", Value: "Siqi", Type: "温柔女声", Note: "支持中文及中英文混合场景"},
 	{Name: "思佳", Value: "Sijia", Type: "标准女声", Note: "支持中文及中英文混合场景"},
@@ -102,8 +102,8 @@ var VoiceList = []Voice{
 	{Name: "艾达", Value: "Aida", Type: "标准男声", Note: "支持中文及中英文混合场景"},
 	{Name: "宁儿", Value: "Ninger", Type: "标准女声", Note: "仅支持纯中文场景"},
 	{Name: "瑞琳", Value: "Ruilin", Type: "标准女声", Note: "仅支持纯中文场景"},
-	{Name: "阿美", Value: "Amei", Type: "甜美女声", Note: "支持中文及中英文混合场景"},
-	{Name: "小雪", Value: "Xiaoxue", Type: "温柔女声", Note: "支持中文及中英文混合场景"},
+	{Name: "阿美", Value: "Amei", Type: "甜美女声", Note: "支持中文及中英文混合场景"},    //del
+	{Name: "小雪", Value: "Xiaoxue", Type: "温柔女声", Note: "支持中文及中英文混合场景"}, //del
 	{Name: "思悦", Value: "Siyue", Type: "温柔女声", Note: "支持中文及中英文混合场景"},
 	{Name: "艾雅", Value: "Aiya", Type: "严厉女声", Note: "支持中文及中英文混合场景"},
 	{Name: "艾夏", Value: "Aixia", Type: "亲和女声", Note: "支持中文及中英文混合场景"},
@@ -120,7 +120,7 @@ var VoiceList = []Voice{
 	{Name: "艾彤", Value: "Aitong", Type: "儿童音", Note: "仅支持纯中文场景"},
 	{Name: "艾薇", Value: "Aiwei", Type: "萝莉女声", Note: "仅支持纯中文场景"},
 	{Name: "艾宝", Value: "Aibao", Type: "萝莉女声", Note: "仅支持纯中文场景"},
-	{Name: "Halen", Value: "Halen", Type: "英音女声", Note: "仅支持英文场景"},
+	{Name: "Halen", Value: "Halen", Type: "英音女声", Note: "仅支持英文场景"}, //del
 	{Name: "Harry", Value: "Harry", Type: "英音男声", Note: "仅支持英文场景"},
 	{Name: "Eric", Value: "Eric", Type: "英音男声", Note: "仅支持英文场景"},
 	{Name: "Emily", Value: "Emily", Type: "英音女声", Note: "仅支持英文场景"},
@@ -130,4 +130,25 @@ var VoiceList = []Voice{
 	{Name: "William", Value: "William", Type: "英音男声", Note: "仅支持英文场景"},
 	{Name: "Olivia", Value: "Olivia", Type: "英音女声", Note: "仅支持英文场景"},
 	{Name: "姗姗", Value: "Shanshan", Type: "粤语女声", Note: "支持标准粤文（简体）及粤英文混合场景"},
+	{Name: "Chuangirl", Value: "chuangirl", Type: "四川话女声", Note: "中文及中英文混合场景"},
+	{Name: "Lydia", Value: "lydia", Type: "英中双语女声", Note: "支持标准粤文（简体）及粤英文混合场景"},
+	{Name: "艾硕", Value: "aishuo", Type: "自然男声", Note: "中文及中英文混合场景"},
+	{Name: "青青", Value: "qingqing", Type: "台湾话女声", Note: "中文场景"},
+	{Name: "翠姐", Value: "cuijie", Type: "东北话女声", Note: "中文场景"},
+	{Name: "小泽", Value: "xiaoze", Type: "湖南重口音男声", Note: "中文场景"},
+	{Name: "智香", Value: "tomoka", Type: "日语女声", Note: "日文场景"},
+	{Name: "智也", Value: "tomoya", Type: "日语男声", Note: "日文场景"},
+	{Name: "Annie", Value: "annie", Type: "美语女声", Note: "英文场景"},
+	{Name: "佳佳", Value: "jiajia", Type: "粤语女声", Note: "标准粤文（简体）及粤英文混合场景"},
+	{Name: "Indah", Value: "印尼语女声", Type: "印尼语女声", Note: "支持标准粤文及粤英文混合场景"},
+	{Name: "桃子", Value: "taozi", Type: "粤语女声", Note: "支持标准粤文（简体）及粤英文混合场景"},
+	{Name: "柜姐", Value: "guijie", Type: "亲切女声", Note: "支持中文及中英文混合场景"},
+	{Name: "Stella", Value: "stella", Type: "知性女声", Note: "支持中文及中英文混合场景"},
+	{Name: "Stanley", Value: "stanley", Type: "沉稳男声", Note: "支持中文及中英文混合场景"},
+	{Name: "Kenny", Value: "kenny", Type: "沉稳男声", Note: "支持中文及中英文混合场景"},
+	{Name: "Rosa", Value: "rosa", Type: "自然女声", Note: "支持中文及中英文混合场景"},
+	{Name: "Farah", Value: "farah", Type: "马来语女声", Note: "仅支持纯马来语场景"},
+	{Name: "马树", Value: "mashu", Type: "儿童剧男声", Note: "支持中文及中英文混合场景"},
+	{Name: "小仙", Value: "xiaoxian", Type: "亲切女声", Note: "支持中文及中英文混合场景"},
+	{Name: "悦儿", Value: "yuer", Type: "儿童剧女声", Note: "仅支持纯中文场景"},
 }
